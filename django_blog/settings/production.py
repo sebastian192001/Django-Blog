386bfa10c351408d68305f4972c3c8b3b061fef8
unknown <jnavarreterodriguez19@gmail.com>
2020-11-10 22:48:05 -0500

    credenciales de bd de heroku añadidas

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -10,7 +10,12 @@ ALLOWED_HOSTS = ['djangoblog19.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dolkv78kmnevp',
+        'USER': 'ozhjybuqciists',
+        'PASSWORD': '3143155eadb18d4a3ce5ec22da7df4646d91908725c3c44d811356fb967eb57e',
+        'HOST': 'ec2-23-20-70-32.compute-1.amazonaws.com',
+        'PORT': 5432,
+
     }
 }
